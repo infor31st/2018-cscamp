32fb696b09793be709efc37fb307429f4b74ffb7
Tony Yang <t510599@gmail.com>
2020-12-01 17:42:11 +0000

    rendered static files for gh-pages

--- db_export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mongoexport -d 2018-cscamp -c register --type=csv -o register.csv -f fullname,gender,size,school,phone,parentName,transport,email,id,birthday,emergencyContact,parentalConsent,message,time
-zip -r -0 2018_cscamp_registration.zip upload/ register.csv
